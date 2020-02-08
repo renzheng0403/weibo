16f03268285d1d232b787ab71e4dc26bf010b1d5
renzheng <renz@koal.com>
2020-02-08 22:42:32 +0800

    用户注册表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,25 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        /**
+         * 使用 validate 方法进行数据验证，接收两个参数
+         * 第一个参数：用户输入的数据
+         * 第二个参数：该输入数据的验证规则
+         */
+        /**
+         * require：存在性验证，验证是否为空
+         * min 和 max：长度验证，限制最小长度和最大长度
+         * email：格式验证，邮箱格式
+         * unique：唯一性验证
+         */
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
