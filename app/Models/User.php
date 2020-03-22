f488678e9d2f9d7169e4eef325ada1e5751b3f9c
renzheng0403 <191162792@qq.com>
2020-03-22 14:36:36 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 用于生成用户头像
+     *
+     * @var string
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
