2e40d0025c1ebc89d74c64795ac4220865673c17
renzheng0403 <191162792@qq.com>
2020-03-22 19:03:32 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,6 +39,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($users);
+
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
